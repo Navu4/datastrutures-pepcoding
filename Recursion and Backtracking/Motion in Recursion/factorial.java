f923a2b824a72e54a9b222829a7f10cc5003b33f
Navu4 <51900952+Navu4@users.noreply.github.com>
2021-03-26 01:16:07 +0530

    Update Recursion in Motion

---
+++ Recursion and Backtracking/Motion in Recursion/factorial.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static Scanner scn = new Scanner(System.in);
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        int n = scn.nextInt();
+        System.out.print(factorial(n));
+    }
+
+    public static int factorial(int n) {
+        if(n <= 1) return 1;
+        
+        return n*factorial(n-1);
+    }
+
+}
---
+++ Recursion and Backtracking/Motion in Recursion/printZigZag.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static Scanner scn = new Scanner(System.in);
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        int n = scn.nextInt();
+        pzz(n);
+    }
+
+    public static void pzz(int n) {
+        if (n < 1) return;
+
+        System.out.print(n + " ");
+        pzz(n - 1);
+        System.out.print(n + " ");
+        pzz(n - 1);
+        System.out.print(n + " ");
+    }
+
+}
